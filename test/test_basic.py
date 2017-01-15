05106d335b31f57fea64be722c29fa1884bd3a70
Falldog <falldog@hardcoretech.co>
2017-01-15 14:40:31 +0800

    (Py3) Fix unicode/byte error in python3

--- test/test_basic.py
+++ test/test_basic.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import unicode_literals
 
 import base
 import unittest
@@ -21,14 +22,15 @@ import unittest
 class TestBasic(base.TestPyConcreteBase):
     def test_import(self):
         import pyconcrete
-        print pyconcrete.info()
+
+        print('pyconcrete info="%s", path=%s' % (pyconcrete.info(), pyconcrete.__file__))
 
         # print "lib dir=[%s], pyconcrete __init__=[%s]" % (self.lib_dir, pyconcrete.__file__)
         self.assertTrue(pyconcrete.__file__.startswith(self.lib_dir))  # check import correct module
 
     def test_import_pye(self):
-        py_code = "v_int=1\r\n"
-        py_code += "v_string='abc'\r\n"
+        py_code = b"v_int=1\r\n"
+        py_code += b"v_string='abc'\r\n"
         pye_filepath = self.lib_gen_pye(py_code, 'test_module.pye')
 
         # import test.pye testing
--- test/test_concrete.py
+++ test/test_concrete.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import unicode_literals
 
 import base
 import unittest
@@ -40,34 +41,34 @@ class TestConcrete(base.TestPyConcreteBase):
         return pyconcrete.decrypt_buffer(data)
 
     def test_process_py_code_empty(self):
-        py_code = ''
+        py_code = b''
         res = self.__do_encrypt_decrypt_file(py_code)
         self.assertEqual(py_code, res)
 
     def test_process_py_code_large(self):
         py_code = ''
-        for i in xrange(100):
-            py_code += 'print "This is testing large py file ... %d"\r\n' % i
-        res = self.__do_encrypt_decrypt_file(py_code)
-        self.assertEqual(py_code, res)
+        for i in range(100):
+            py_code += ('print("This is testing large py file ... %s")\r\n' % i)
+        res = self.__do_encrypt_decrypt_file(py_code.encode('utf8'))
+        self.assertEqual(py_code, res.decode('utf8'))
 
     def test_process_py_code_1_block(self):
-        py_code = 'v=12345678901234'
+        py_code = b'v=12345678901234'
         self.assertEqual(16, len(py_code))  # 1 block
         
         res = self.__do_encrypt_decrypt_file(py_code)
         self.assertEqual(py_code, res)
 
     def test_process_py_code_less_1_block(self):
-        py_code = 'v=123456789'
+        py_code = b'v=123456789'
         self.assertLess(len(py_code), 16)  # less than 1 block
         
         res = self.__do_encrypt_decrypt_file(py_code)
         self.assertEqual(py_code, res)
 
     def test_process_py_code_2_block(self):
-        py_code = 'v1=12345678901\r\n'
-        py_code += 'v2=12345678901\r\n'
+        py_code = b'v1=12345678901\r\n'
+        py_code += b'v2=12345678901\r\n'
         self.assertEqual(len(py_code), 32)  # 2 blocks
         
         res = self.__do_encrypt_decrypt_file(py_code)
