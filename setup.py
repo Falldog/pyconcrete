9e8cb2c9ff73526422401e965b536720bc9b85eb
Falldog <falldog@hardcoretech.co>
2021-09-28 09:28:09 +0800

    support python 3.8

--- setup.py
+++ setup.py
@@ -424,6 +424,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: Apache Software License',
     ],
