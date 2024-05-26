d6f17b63d147bf8b648e5a20441cda443d282864
Falldog <falldog@gofreight.com>
2024-05-26 18:32:46 +0800

    feat(django-example): upgrade to django 5.x

--- example/django/pye_web/pye_web/urls.py
+++ example/django/pye_web/pye_web/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path(r'admin/', admin.site.urls),
 ]
