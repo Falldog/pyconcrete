ba717ff9a25a88b18b6268e808529b3c97e59359
Falldog <falldog@gofreight.com>
2025-02-17 19:14:09 +0800

    feat(meson): add dist_cleanup to remove redundant files in sdist

---
+++ meson_utility/dist_cleanup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIST_DIR='meson-dist/pyconcrete-*'
+
+echo "clean up the dist files in '${DIST_DIR}'"
+
+# this script will be executed when do `python -m build --sdist`
+# and this will cleanup the meson staging folder
+
+set -ex
+
+rm -rf ${DIST_DIR}/appveyor/
+rm -rf ${DIST_DIR}/bin/
+rm -rf ${DIST_DIR}/docker/
+rm -rf ${DIST_DIR}/example/
+rm -rf ${DIST_DIR}/test_legacy/
+rm -rf ${DIST_DIR}/tests/
