0274a16d7cc7c56d307fb14caa5a89ddb7514045
Falldog <falldog@gofreight.com>
2025-07-24 13:22:17 +0800

    fix(example): revise the exception env & create superuser

--- bin/run-example-django.sh
+++ bin/run-example-django.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 PORT=${PORT:="5151"}
+NAME=${NAME:="admin"}
+PW=${PW:="1234"}
 
 declare -xr REPO_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
 
@@ -11,9 +13,16 @@ cd ${REPO_ROOT}
 docker build \
     -t falldog/example-django \
     -f example/django/Dockerfile \
+    --build-arg NAME=${NAME} \
+    --build-arg PW=${PW} \
     .
 
 echo "Django testing website running at http://127.0.0.1:${PORT}"
+echo "You also able to browse Django admin page http://127.0.0.1:${PORT}/admin"
+echo "Name: ${NAME}"
+echo "Password: ${PW}"
+echo ""
+
 docker run \
     --rm \
     -p ${PORT}:80 \
